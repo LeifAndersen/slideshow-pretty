8d239e6c4e60adf3297bba317664201187b33785
Leif Andersen <leif@leifandersen.net>
2014-03-27 23:13:03 -0600

    Initial Commit.

---
+++ info.rkt
@@ -0,0 +1,5 @@
+#lang setup/infotab
+(define collection 'multi)
+(define setup-collects '("slideshow/pretty"))
+(define deps '("base" "draw-lib" "gui-lib" "slideshow-lib"))
+(define build-deps '("racket-doc" "scribble-lib" "slideshow-doc"))
