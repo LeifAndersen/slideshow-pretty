e1514012bbe8d28bbcdf0b76105bf93b1f0a7ca3
Leif Andersen <leif@leifandersen.net>
2014-03-31 20:21:02 -0600

    Remove collects.

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define version "0.0")
 
 (define collection 'multi)
-(define setup-collects '("slideshow/pretty"))
+;(define setup-collects '("slideshow/pretty"))
 (define deps '("base" "draw-lib" "gui-lib" "slideshow-lib"))
 (define build-deps '("racket-doc" "scribble-lib" "slideshow-doc"))
