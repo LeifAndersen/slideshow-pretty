fc0fbc213f3cbe8bc1aa90327a3a5e5b4f35610b
Leif Andersen <leif@leifandersen.net>
2014-08-06 18:19:11 -0400

    Fixed dependancies.

--- info.rkt
+++ info.rkt
@@ -3,5 +3,5 @@
 (define version "0.0")
 
 (define collection 'multi)
-(define deps '("base" "draw-lib" "gui-lib" "slideshow-lib"))
-(define build-deps '("racket-doc" "scribble-lib" "slideshow-doc" "slideshow-latex"))
+(define deps '("base" "draw-lib" "gui-lib" "slideshow-lib" "slideshow-latex"))
+(define build-deps '("racket-doc" "scribble-lib" "slideshow-doc"))
